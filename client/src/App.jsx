c4d1f1092b1e6d6218ed3c1471cb8630e988996c
Ameen <ameencrews@example.com>
2023-09-27 20:07:54 +0530

    u

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,18 +3,21 @@ import { Fragment } from "react"
 
 import { Mainpage } from "./Componets/page/Mainpage"
 import { Productlist } from "./Componets/Productlist/Productlist"
+import { Route, Routes } from "react-router"
+
 
 
 
 function App() {
   return (
-     <Fragment>
-      <Routes>
-        <Route path="/" element={<Mainpage/>}/>
-        <Route path="/product/:id" element={<Productlist/>} />
-      </Routes>
-       
-     </Fragment>
+     <div>
+     <Routes>
+     <Route path="/" element={<Mainpage/>}/>
+      <Route path="/product/:id" element={<Productlist/>}/>
+     </Routes>
+
+    </div>
+     
   )
 }
 
